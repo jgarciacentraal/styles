2ec72739226ebd5e1c18745c515ab758e9c532d9
Julio Estefanes <jsgr1992@gmail.com>
2021-09-16 17:52:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Create some styles for my proyects
---
+++ styled-components/Card.js
@@ -0,0 +1,17 @@
+//custom card
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 20px 0;
+`;
+
+export const CardSignIn = styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  height: 400px;
+  width: 330px;
+  background-color: white;
+  border-radius: 10px;
+  border: 1px solid #ddd;
+  margin: 0 auto;
+  padding: 20px;
+`;
